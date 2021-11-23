8691863ba2ab4abc5b7250c4b70114dbf978068e
jhhonr144 <jhhonr144@gmail.com>
2021-11-22 21:43:25 -0500

    Api  crear producto falta configurar imagen

---
+++ Backend/src/entities/Products.ts
@@ -0,0 +1,34 @@
+export interface IProducts {
+    id_uid: string;
+    id_product :string;
+    name: string;
+    category: number;
+    marking:string; 
+    ticket:number;
+    Inventory: number;
+    hours_create : string
+    date_create : string;
+    state : number;
+
+  
+}
+
+class Products implements IProducts {
+
+    id_uid: string;
+    id_product :string;
+    name: string;
+    category: number;
+    marking:string; 
+    ticket:number;
+    Inventory: number;
+    hours_create : string
+    date_create : string;
+    state : number;
+    
+    constructor() {
+
+    }
+}
+
+export default Products;
---
+++ Backend/src/routes/controllers/productCtrl.ts
@@ -0,0 +1,88 @@
+import StatusCodes from 'http-status-codes';
+import { Request, Response } from 'express';
+import * as admin from 'firebase-admin';
+
+import { paramMissingError } from 'shared/constants';
+import Products from 'entities/Products';
+
+
+const { BAD_REQUEST, CREATED, OK } = StatusCodes;
+const collection:string = "Products";
+
+export async function getAll(req: Request, res: Response) {
+    const result = await admin.firestore().collection(collection).get();
+    const list: any[] = [];
+
+    result.forEach(doc => { 
+        list.push({
+            id: doc.id, 
+            data: doc.data()
+        });
+    });
+    
+    return res.status(OK).json(list);
+}
+
+//buscar por producto individual
+export async function get(req: Request, res) {
+    try {
+        let obj:any;
+        const id_product = req.params.id_product;
+        const query = await admin.firestore().collection(collection).where("id_product","==",id_product).get();
+
+        query.forEach(querySnapshot =>{
+            obj = querySnapshot.data();
+            //console.log(querySnapshot.data())
+        } 
+           
+        );
+           
+            
+        return res.status(OK).json(obj);
+       
+      
+    } catch (err) {
+        return handleError(res, err)
+    }
+ }
+
+
+
+export async function add(req: Request, res: Response) {
+    const newItem: Products = {...req.body};
+    const newDoc = await admin.firestore().collection(collection).add(newItem);
+    //console.log(`Created a new ${collection}: ${newDoc.id}`);
+    newItem.id_product = newDoc.id
+    return res.status(CREATED).send(newItem);
+}
+
+
+export async function update(req: Request, res: Response) {
+    try{
+        const { id } = req.params
+        const newItem: Products = {...req.body};
+
+        const ref = admin.firestore().collection(collection).doc(id);
+
+        // Set the 'capital' field of the city
+        const result = await ref.update(newItem);
+
+        return res.status(OK).json({message:"Ticket updated!"});
+    } catch (error) {
+        res.status(400).send(JSON.stringify(error));
+    }
+}
+
+export async function remove(req: Request, res: Response) {
+    try {
+        const { id } = req.params;
+        await admin.firestore().collection(collection).doc(id).delete();
+        return res.status(OK).end()
+    } catch (err) {
+        return handleError(res, err)
+    }
+}
+
+function handleError(res: Response, err: any) {
+    return res.status(500).send({ message: `${err.code} - ${err.message}` });
+}
